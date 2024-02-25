93ead0e05725f6a51a07df57ee2474bdb0d1b6a9
leary <leary1@yandex.ru>
2024-02-26 00:15:39 +0600

    Carousel Slider added

--- lib/pages/forecast_page.dart
+++ lib/pages/forecast_page.dart
@@ -1,3 +1,4 @@
+import 'package:carousel_slider/carousel_slider.dart';
 import 'package:flutter/material.dart';
 
 class ForecastPage extends StatelessWidget {
@@ -5,10 +6,28 @@ class ForecastPage extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return Row(
+    return Column(
+      crossAxisAlignment: CrossAxisAlignment.start,
       children: [
         const SizedBox(height: 12),
         Text("Right now", style: Theme.of(context).textTheme.titleLarge),
+        const SizedBox(height: 12),
+        CarouselSlider(
+          items: [
+            Container(
+              color: Colors.amber,
+            ),
+            Container(
+              color: Colors.pink,
+            ),
+          ],
+          options: CarouselOptions(
+            enlargeCenterPage: true,
+            height: 200,
+            enableInfiniteScroll: false,
+            initialPage: 0,
+          ),
+        )
       ],
     );
   }
